bb11be3a07d261a7d23885c1857442093544c9b3
Prakash8999 <jhaprakash504@gmail.com>
2025-12-19 22:16:17 +0530

    made changes

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,69 +1,7 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import { combinedOpenApiDoc } from './common/docs';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-
-// Load environment variables FIRST
-dotenv.config();
+import app from './app';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:8080'], methods: ['GET', 'POST', 'PATCH', 'DELETE'], credentials: true }));
-
-
-
-
-// Import all models to ensure they are registered with Sequelize
-
-// Import routes
-import userRoutes from './modules/users/user.route';
-import assetRoutes from './modules/assets/asset.route';
-import noteRoutes from './modules/notes/notes.route';
-import canvasRoute from './modules/canvas/canvas.routes'
-import qcRoute from './modules/quick-capture/qc.routes'
-import aiRoute from './modules/ai/ai.route';
-// Use routes
-
-import redisClient, { connectRedis } from './config/redis';
-async function initApp() {
-  try {
-    await connectRedis();
-    const result = await redisClient.ping();
-    console.log('Redis is working! PING =>', result);
-  } catch (err) {
-    console.error('Redis connection failed:', err);
-  }
-}
-import './jobs/otp.worker';
-// later on shutdown:
-// Use a signal handler to close the worker gracefully
-// async function workers() {
-// await otpWorker.close();
-//  otpWorker.close();
-// } 
-// workers()
-initApp();
-
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API from user service' });
-});
-app.use('/api-docs', ...swaggerUi.serve, swaggerUi.setup(combinedOpenApiDoc));
-
-const apiV1 = '/api/v1'
-app.use(`${apiV1}/user`, userRoutes);
-app.use(`${apiV1}/assets`, assetRoutes);
-app.use(`${apiV1}/note`, noteRoutes);
-app.use(`${apiV1}/canvas`, canvasRoute)
-app.use(`${apiV1}/quick-capture`, qcRoute)
-app.use(`${apiV1}/ai`, aiRoute);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
